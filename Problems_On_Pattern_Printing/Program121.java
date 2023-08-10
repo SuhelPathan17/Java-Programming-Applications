750ae5c47c2e2baef0c10b37721423a264d1f030
SuhelPathan17 <119145802+SuhelPathan17@users.noreply.github.com>
2023-08-10 17:17:28 +0530

    Add files via upload

---
+++ Problems_On_Pattern_Printing/Program121.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+class Pattern
+{
+    public int iRow;
+    public int iCol;
+
+    public Pattern(int r, int c)
+    {
+        iRow = r;
+        iCol = c;
+    }
+
+    public void DisplayPattern()
+    {        
+        int i = 0;
+        int j = 0;
+
+        if(iRow < 0)
+        {
+            iRow = -iRow;
+        }
+
+        if(iCol < 0)
+        {
+            iCol = -iCol;
+        }
+
+        for(i = 1; i<=iRow; i++)
+        {
+            for(j = 1; j<=iCol; j++)
+            {
+                if(i > j)
+                {
+                    System.out.print("*\t");
+                }
+                else
+                {
+                    System.out.print(" \t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Program121
+{
+    public static void main(String ar[])
+    {
+        int iNo1 = 0;
+        int iNo2 = 0;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the row size:");
+        iNo1 = sobj.nextInt();
+        System.out.println("Enter the column size:");
+        iNo2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern(iNo1,iNo2);
+        pobj.DisplayPattern();
+    }
+}
